3300cfeb17ee5445e9ddb728829e3717db2c4374
ujwal dhakal <kevin.ujwal@gmail.com>
2019-11-16 20:44:51 +0545

    added fixer support

--- app/Datasource/RolesData.php
+++ app/Datasource/RolesData.php
@@ -7,18 +7,17 @@ use App\Exceptions\DataNotLoaded;
 
 class RolesData implements RolesDataInterface
 {
-    private function getFileName(): string
-    {
-        return "app/Datasource/roles.json";
-    }
-
     public function getAll(): array
     {
         try {
-
             return json_decode(file_get_contents($this->getFileName()), 1);
         } catch (\Exception $exception) {
             throw new DataNotLoaded();
         }
+    }
+
+    private function getFileName(): string
+    {
+        return 'app/Datasource/roles.json';
     }
 }
