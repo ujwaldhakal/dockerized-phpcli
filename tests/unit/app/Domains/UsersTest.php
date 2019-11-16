db695583a13a42c61c319578a8384a20609a3a1e
ujwal dhakal <kevin.ujwal@gmail.com>
2019-11-17 03:24:54 +0545

    added full test coverage for subordinate search

--- tests/unit/app/Domains/UsersTest.php
+++ tests/unit/app/Domains/UsersTest.php
@@ -5,17 +5,31 @@ use App\Datasource\Interfaces\UsersDataInterface;
 use App\Datasource\RolesData;
 use App\Datasource\UsersData;
 use App\Domains\Users;
-use Tests\app\Domains;
+use PHPUnit\Framework\TestCase;
 
-class UsersTest extends \PHPUnit\Framework\TestCase
+class UsersTest extends TestCase
 {
-
     public function testIfGetSubordinatesWorksWell()
     {
         $user = new Users($this->mockedUserData(), $this->mockedRolesData());
-        $this->assertIsArray($user->getSubordinates(3));
-    }
+        $expectedData = [
+            ['id' => 2, 'name' => 'Emily Employee', 'role' => 4],
+            ['id' => 5, 'name' => 'Steve Trainer', 'role' => 5],
+        ];
 
+        $this->assertSame($this->sortMultiDimenArrays($expectedData), $this->sortMultiDimenArrays($user->getSubordinates(3)));
+
+        $this->assertEmpty($user->getSubordinates(2));
+
+        $expectedData = [
+            ['id' => 2, 'name' => 'Emily Employee', 'role' => 4],
+            ['id' => 3, 'name' => 'Sam Supervisor', 'role' => 3],
+            ['id' => 4, 'name' => 'Mary Manager', 'role' => 2],
+            ['id' => 5, 'name' => 'Steve Trainer', 'role' => 5],
+        ];
+
+        $this->assertSame($this->sortMultiDimenArrays($expectedData), $this->sortMultiDimenArrays($user->getSubordinates(1)));
+    }
 
     public function testIfSearchUserByIdWorks()
     {
@@ -29,7 +43,6 @@ class UsersTest extends \PHPUnit\Framework\TestCase
 
         $actualData = $this->invokeMethod($user, 'searchUserById', [10]);
         $this->assertEmpty($actualData);
-
     }
 
     public function testIfSearchChildrenRoleByRoleIdWorks()
@@ -47,25 +60,8 @@ class UsersTest extends \PHPUnit\Framework\TestCase
         $this->assertEmpty($actualData);
     }
 
-
-    /**
-     * Just to make sure our expectation & actual array remains same for compare
-     * @param $array
-     * @return mixed
-     */
-    private function sortMultiDimenArrays($array)
-    {
-        usort($array, function ($a, $b) {
-            return $a > $b;
-        });
-
-        return $array;
-    }
-
     public function testIfSearchUserByRoleIdsWorks()
     {
-
-
         $user = new Users($this->mockedUserData(), $this->mockedRolesData());
         $actualData = $this->invokeMethod($user, 'searchUserByRoleIds', [[3, 4]]);
 
@@ -82,20 +78,18 @@ class UsersTest extends \PHPUnit\Framework\TestCase
         ];
 
         $this->assertSame($this->sortMultiDimenArrays($expectedData), $this->sortMultiDimenArrays($actualData));
-
-
     }
 
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object Instantiated object that we will run method on.
+     * @param object &$object    Instantiated object that we will run method on
      * @param string $methodName Method name to call
-     * @param array $parameters Array of parameters to pass into method.
+     * @param array  $parameters array of parameters to pass into method
      *
-     * @return mixed Method return.
+     * @return mixed method return
      */
-    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
     {
         $reflection = new \ReflectionClass(get_class($object));
         $method = $reflection->getMethod($methodName);
@@ -105,8 +99,25 @@ class UsersTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * Just to make sure our expectation & actual array remains same for compare.
+     *
+     * @param $array
+     *
+     * @return mixed
+     */
+    private function sortMultiDimenArrays($array)
+    {
+        usort($array, function ($a, $b) {
+            return $a > $b;
+        });
+
+        return $array;
+    }
+
+    /**
      * We could use a separate set of mocked data here
-     * I am using same sort of input data to make it look easier
+     * I am using same sort of input data to make it look easier.
+     *
      * @return array
      */
     private function mockedUserData(): UsersDataInterface
@@ -118,6 +129,4 @@ class UsersTest extends \PHPUnit\Framework\TestCase
     {
         return new RolesData();
     }
-
-
 }
